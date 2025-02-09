02aa3f02286d69dd731617ad05d74148feb11c8a
Dominic Price <dominic.price@nottingham.ac.uk>
2025-02-09 18:07:51 +0000

    Reorder imports

--- src/recorder.cpp
+++ src/recorder.cpp
@@ -18,12 +18,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <array>
 #include <atomic>
 #include <csignal>
-#include <fcntl.h>
 #include <functional>
 #include <iostream>
 #include <mutex>
-#include <sys/stat.h>
 #include <thread>
+
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #include <franka/exception.h>
