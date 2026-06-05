f33a8a3e338ebc34d4aa596906caa76753ae8d05
Dominic Price <dominic.price@nottingham.ac.uk>
2026-06-05 23:19:06 +0100

    read grasp parameters from command line

--- src/grasp.cpp
+++ src/grasp.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <cxxopts.hpp>
+
 #include <franka/exception.h>
 #include <franka/gripper.h>
 
@@ -9,11 +11,22 @@ int main(int argc, const char** argv) {
 
   YAML::Node config = parse_options(argc, argv);
 
+  cxxopts::Options options(argv[0]);
+  options.add_options()
+    ("w,width", "Grasp width", cxxopts::value<double>());
+  options.add_options()
+    ("s,speed", "Grasp speed", cxxopts::value<double>());
+  options.add_options()
+    ("f,force", "Grasp force", cxxopts::value<double>());
+  auto poptions = options.parse(argc, argv);
+
   try {
     franka::Gripper gripper(config["robot"]["host"].as<std::string>());
-    double width = 0.024;
+    double width = poptions["width"].as<double>();
+    double speed = poptions["speed"].as<double>();
+    double force = poptions["force"].as<double>();
     gripper.homing();
-    gripper.grasp(width, 0.05, 70);
+    gripper.grasp(width, speed, force);
   }
   catch (franka::Exception const& ex) {
     std::cerr << "Error: " << std::endl << ex.what() << std::endl;
