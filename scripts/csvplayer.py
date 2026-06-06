e51a80aaac4fcd437bb9401a44e77671d7edefe5
Dominic Price <dominic.price@nottingham.ac.uk>
2026-06-06 21:52:38 +0100

    add command line argument for skipping header file

--- scripts/csvplayer.py
+++ scripts/csvplayer.py
@@ -2,6 +2,7 @@ import argparse
 import csv
 import pathlib
 import socket
+import sys
 import time
 
 import capnp
@@ -16,15 +17,40 @@ robotstate_capnp = capnp.load(
 )
 
 
-def play(file: pathlib.Path, frequency: int, host: str, port: int):
+def play(file: pathlib.Path, frequency: int, host: str, port: int, skip_header: bool):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     with open(file, "r") as f:
         csvreader = csv.reader(f, delimiter=",", quotechar='"')
-        next(csvreader)  # Skip header line
+
+        if skip_header:
+            next(csvreader)
+
         first_row = True
 
+        state = robotstate_capnp.RobotState()
+        state.joint1Vel = 0
+        state.joint2Vel = 0
+        state.joint3Vel = 0
+        state.joint4Vel = 0
+        state.joint5Vel = 0
+        state.joint6Vel = 0
+        state.joint7Vel = 0
+        state.joint1Torque = 0
+        state.joint2Torque = 0
+        state.joint3Torque = 0
+        state.joint4Torque = 0
+        state.joint5Torque = 0
+        state.joint6Torque = 0
+        state.joint7Torque = 0
+        state.joint1ExtTorque = 0
+        state.joint2ExtTorque = 0
+        state.joint3ExtTorque = 0
+        state.joint4ExtTorque = 0
+        state.joint5ExtTorque = 0
+        state.joint6ExtTorque = 0
+        state.joint7ExtTorque = 0
+
         for row in csvreader:
-            state = robotstate_capnp.RobotState()
             state.time = int(time.time() * 1000)
             state.joint1Pos = float(row[0])
             state.joint2Pos = float(row[1])
@@ -33,27 +59,6 @@ def play(file: pathlib.Path, frequency: int, host: str, port: int):
             state.joint5Pos = float(row[4])
             state.joint6Pos = float(row[5])
             state.joint7Pos = float(row[6])
-            state.joint1Vel = 0
-            state.joint2Vel = 0
-            state.joint3Vel = 0
-            state.joint4Vel = 0
-            state.joint5Vel = 0
-            state.joint6Vel = 0
-            state.joint7Vel = 0
-            state.joint1Torque = 0
-            state.joint2Torque = 0
-            state.joint3Torque = 0
-            state.joint4Torque = 0
-            state.joint5Torque = 0
-            state.joint6Torque = 0
-            state.joint7Torque = 0
-            state.joint1ExtTorque = 0
-            state.joint2ExtTorque = 0
-            state.joint3ExtTorque = 0
-            state.joint4ExtTorque = 0
-            state.joint5ExtTorque = 0
-            state.joint6ExtTorque = 0
-            state.joint7ExtTorque = 0
 
             sock.sendto(state.to_bytes(), (host, port))
 
@@ -64,7 +69,7 @@ def play(file: pathlib.Path, frequency: int, host: str, port: int):
                 time.sleep(1 / frequency)
 
 
-def main():
+def main(args: list[str]):
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument(
         "-h", "--host", help="Robot host/IP address", type=str, required=True
@@ -73,12 +78,16 @@ def main():
         "-p", "--port", help="Robot port number", type=int, required=True
     )
     parser.add_argument(
-        "-f", "--frequency", help="Playback frequency", type=int, required=True
+        "-f", "--frequency", help="Playback frequency (Hz)", type=int, required=True
+    )
+    parser.add_argument(
+        "-s", "--skip-header", help="Skip first line of CSV file", action="store_true"
     )
     parser.add_argument("csvfile", help="Path to CSV file to play from", type=str)
-    args = parser.parse_args()
-    play(args.csvfile, args.frequency, args.host, args.port)
+    pargs = parser.parse_args(args=args)
+    play(pargs.csvfile, pargs.frequency, pargs.host, pargs.port, pargs.skip_header)
 
 
+# TODO: Use readchar lib to interactively send messages
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
